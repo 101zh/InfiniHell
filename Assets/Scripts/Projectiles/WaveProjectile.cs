80e65e77439acddb10b01373855aa14ff4a6c5bf
101zh <67253838+101zh@users.noreply.github.com>
2024-07-13 15:36:42 -0700

    make wave bullet's line travel able to angle

--- Assets/Scripts/Projectiles/WaveProjectile.cs
+++ Assets/Scripts/Projectiles/WaveProjectile.cs
@@ -7,13 +7,16 @@ public class WaveProjectile : Projectile
     [SerializeField] private bool isReflected = false;
     [SerializeField] private float amplitude = 1f;
     [SerializeField] private float period = (2 * Mathf.PI);
+    [SerializeField] private float angle = 90f;
 
+    private float alpha;
     private Vector2 originalPos;
     private float time = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        alpha = Mathf.Deg2Rad * angle;
         amplitude = isReflected ? -1f : 1f;
         originalPos = transform.position;
     }
@@ -29,6 +32,10 @@ public class WaveProjectile : Projectile
     {
         float x = speed * time;
         float b = (2 * Mathf.PI) / period;
-        return originalPos + new Vector2(x / b, amplitude * Mathf.Sin(x));
+        Vector2 pos = originalPos + new Vector2(x / b, amplitude * Mathf.Sin(x));
+
+        float x_prime = pos.x * Mathf.Cos(alpha) - pos.y * Mathf.Sin(alpha);
+        float y_prime = pos.x * Mathf.Sin(alpha) + pos.y * Mathf.Cos(alpha);
+        return new Vector2(x_prime, y_prime);
     }
 }
